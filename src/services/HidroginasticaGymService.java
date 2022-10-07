76d23070ed385bdf4e28e96142c758f5cf53e346
juliaomota <juliao.mota@projetusti.com.br>
2022-10-07 15:34:31 -0300

    Criada interface HidroGymService

---
+++ src/services/HidroginasticaGymService.java
@@ -0,0 +1,32 @@
+package services;
+
+import entidades.Hidroginastica;
+
+public interface HidroginasticaGymService {
+
+    Double getValorTotalAulaHidroginasticaByQuartaManha(Hidroginastica hidroginastica);
+
+    Double getValorTotalAulaHidroginasticaByQuartaTarde(Hidroginastica hidroginastica);
+
+    Double getValorTotalAulaHidroginasticaByQuartaNoite(Hidroginastica hidroginastica);
+
+    Double getValorTotalAulaHidroginasticaBySextaManha(Hidroginastica hidroginastica);
+
+    Double getValorTotalAulaHidroginasticaBySextaTarde(Hidroginastica hidroginastica);
+
+    Double getValorTotalAulaHidroginasticaBySextaNoite(Hidroginastica hidroginastica);
+
+    Double getValorTotalAulaHidroginasticaBySabadoManha(Hidroginastica hidroginastica);
+
+    Double getValorTotalAulaHidroginasticaBySabadoTarde(Hidroginastica hidroginastica);
+
+    Double getValorTotalAulaHidroginasticaBySabadoNoite(Hidroginastica hidroginastica);
+
+    Double getValorTotalAulaHidroginasticaByQuarta(Hidroginastica hidroginastica);
+
+    Double getValorTotalAulaHidroginasticaBySexta(Hidroginastica hidroginastica);
+
+    Double getValorTotalAulaHidroginasticaBySabado(Hidroginastica hidroginastica);
+
+    Double getValorTotalAulaHidroginasticaByWeek(Hidroginastica hidroginastica);
+}
