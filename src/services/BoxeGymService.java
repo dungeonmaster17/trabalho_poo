a6e75537f5e7cecd13cc2f03e50107c69b85dda4
Gica3008 <giovannasilvaramosdesiqueira@gmail.com>
2022-10-07 15:32:38 -0300

    Criada interface BoxeGymService

---
+++ src/services/BoxeGymService.java
@@ -0,0 +1,32 @@
+package services;
+
+import entidades.Boxe;
+
+public interface BoxeGymService {
+
+    Double getValorTotalAulaBoxeByQuartaManha(Boxe boxe);
+
+    Double getValorTotalAulaBoxeByQuartaTarde(Boxe boxe);
+
+    Double getValorTotalAulaBoxeByQuartaNoite(Boxe boxe);
+
+    Double getValorTotalAulaBoxeBySextaManha(Boxe boxe);
+
+    Double getValorTotalAulaBoxeBySextaTarde(Boxe boxe);
+
+    Double getValorTotalAulaBoxeBySextaNoite(Boxe boxe);
+
+    Double getValorTotalAulaBoxeBySabadoManha(Boxe boxe);
+
+    Double getValorTotalAulaBoxeBySabadoTarde(Boxe boxe);
+
+    Double getValorTotalAulaBoxeBySabadoNoite(Boxe boxe);
+
+    Double getValorTotalAulaBoxeByQuarta(Boxe boxe);
+
+    Double getValorTotalAulaBoxeBySexta(Boxe boxe);
+
+    Double getValorTotalAulaBoxeBySabado(Boxe boxe);
+
+    Double getValorTotalAulaBoxeByWeek(Boxe boxe);
+}
